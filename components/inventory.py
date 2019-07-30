0966010ab2d9212bcc381e82354e0d956b882475
PropFeds <propfeds@gmail.com>
2019-07-30 07:02:31 +0700

    god damn pylint

--- components/inventory.py
+++ components/inventory.py
@@ -1,3 +1,4 @@
+#pylint: disable=no-member
 import tcod as libtcod
 from game_messages import Message
 
@@ -14,3 +15,18 @@ class Inventory:
             results.append({'item_added': item, 'message': Message('You pick up the {0}.'.format(item.name), libtcod.lighter_blue)})
             self.contents.append(item)
         return results
+
+    def use_item(self, item_entity, **kwargs):
+        results=[]
+        if item_entity.item.use_function is None:
+            results.append({'message': Message('You don\'t see a way to use the {0}.'.format(item_entity.name), libtcod.yellow)})
+        kwargs={**item_entity.item.function_kwargs, **kwargs}
+        item_use_results=item_entity.item.use_function(self.owner, **kwargs)
+        for result in item_use_results:
+            if result.get('consumed'):
+                self.remove_item(item_entity)
+        results.extend(item_use_results)
+        return results
+
+    def remove_item(self, item):
+        self.items.remove(item)
